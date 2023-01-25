71aa54ccef08aca0aa8eb97d36cc9773c1042faa
Pouria Modaresi <pouria@ifiveo.com>
2023-01-25 19:24:55 +0000

    test

--- models/staging/int1.sql
+++ models/staging/int1.sql
@@ -2,12 +2,13 @@
 
 with cte as (select *,
         EXTRACT(HOUR FROM end_time) AS Hour,
-        EXTRACT(minute FROM end_time) AS Minute,  
+        EXTRACT(minute FROM end_time) AS Minute,
+        EXTRACT(second from end_time) as second,  
         DATE(end_time) as Date
 from {{ ref('int0')}}),
 
 cte1 as (select * from cte where date in ('2023-01-04','2023-01-05','2023-01-07')),
-cte2 as (select camera,status,date,hour,minute from cte1 where camera in ('w2c3', 'w2c4', 'w3c5', 'w3c6', 'w4c7', 'w4c8')),
-cte3 as(select camera,status,date,hour,minute,count(*) as total_second from cte2 group by camera,status,date,hour,minute)
-select camera,status,date,hour,sum(total_second)/60 as total_minute from cte3 group by camera,status,date,hour
+cte2 as (select camera,status,date,hour,minute,second from cte1 where camera in ( 'w4c7', 'w4c8'))
+
+select * from cte2
          
